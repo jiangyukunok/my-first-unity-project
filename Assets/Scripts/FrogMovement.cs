653b7bf61ab6fd8bbe1052531fefe668a01c805f
jiangyukunok <yj297@cornell.edu>
2017-09-04 22:42:31 +0100

    make frog only jump when grounded

--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class FrogMovement : MonoBehaviour {
 	public float jumpElevationInDegrees = 45;
 	public float jumpSpeedInMPS = 5;
+	public float jumpSpeedTolerance = 5;
+	public float jumpGroundClearance = 2;
 
 	// Use this for initialization
 	void Start () {
@@ -12,15 +14,19 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Camera camera = GetComponentInChildren<Camera> ();
-		Debug.DrawRay (transform.position, camera.transform.forward, Color.red);
-		var projectLookDirection = Vector3.ProjectOnPlane (camera.transform.forward, Vector3.up);
-		Debug.DrawRay (transform.position, projectLookDirection, Color.blue);
-		var radianToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
-		var unnormalizedJumpDirection = Vector3.RotateTowards (projectLookDirection, Vector3.up, radianToRotate, 0);
-		var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS;
-		Debug.DrawRay (transform.position, jumpVector, Color.green);
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		bool isOnGround = Physics.Raycast (transform.position, -transform.up, jumpGroundClearance);
+		Debug.DrawRay (transform.position, -transform.up * jumpGroundClearance, Color.yellow);
+		var speed = GetComponent<Rigidbody> ().velocity.magnitude;
+		bool isNearStationary = speed < jumpSpeedTolerance;
+		if (GvrViewer.Instance.Triggered && isOnGround && isNearStationary) {
+			Camera camera = GetComponentInChildren<Camera> ();
+			//Debug.DrawRay (transform.position, camera.transform.forward, Color.red);
+			var projectLookDirection = Vector3.ProjectOnPlane (camera.transform.forward, Vector3.up);
+			//Debug.DrawRay (transform.position, projectLookDirection, Color.blue);
+			var radianToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
+			var unnormalizedJumpDirection = Vector3.RotateTowards (projectLookDirection, Vector3.up, radianToRotate, 0);
+			var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS;
+			//Debug.DrawRay (transform.position, jumpVector, Color.green);
 			GetComponent<Rigidbody> ().AddForce (jumpVector, ForceMode.VelocityChange);
 		}
 	}
