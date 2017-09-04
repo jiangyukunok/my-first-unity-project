3adf9fc893731f5c2aafd21139f2f61d99368f64
jiangyukunok <yj297@cornell.edu>
2017-09-04 05:24:05 +0100

    add new scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextScene()
+	{
+		int currentIndex = SceneManager.GetActiveScene().buildIndex;
+		print(currentIndex);
+		SceneManager.LoadScene(currentIndex + 1);
+	}
+}
