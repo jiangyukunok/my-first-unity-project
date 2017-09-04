6cea04f788b6214f562f243144f3a1d3bbf9ca21
jiangyukunok <yj297@cornell.edu>
2017-09-04 21:54:35 +0100

    enable debug drawRay to paint the direction of object

--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -10,6 +10,12 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.DrawRay (transform.position, jumpVector, Color.red);
+		var projectJumpVector = Vector3.ProjectOnPlane (jumpVector, Vector3.up);
+		Debug.DrawRay (transform.position, projectJumpVector, Color.blue);
+		var radianToRotate = Mathf.Deg2Rad * 75;
+		var rotatedJumpVector = Vector3.RotateTowards (projectJumpVector, Vector3.up, radianToRotate, 0);
+		Debug.DrawRay (transform.position, rotatedJumpVector.normalized, Color.green);
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			GetComponent<Rigidbody> ().AddForce (jumpVector, ForceMode.VelocityChange);
 		}
