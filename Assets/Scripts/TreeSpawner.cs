e6c94edde03477c810b3b0efaa03d63d5fd744d7
jiangyukunok <yj297@cornell.edu>
2017-09-06 05:52:35 +0100

    program to generate random terrain

---
+++ Assets/Scripts/TreeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeSpawner : MonoBehaviour {
+	public GameObject TreePrefab;
+	// Use this for initialization
+	void Start () {
+		CreateTree ();
+		CreateTree ();
+		CreateTree ();
+	}
+
+	void CreateTree(){
+		var tree = Instantiate (TreePrefab);
+		tree.transform.parent = transform;
+		//tree.transform.Translate(Random.Range(-50,50), 0, Random.Range(-5,5));
+		tree.transform.localPosition = new Vector3(Random.Range(-50,50), 0, Random.Range(-5,5));
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
