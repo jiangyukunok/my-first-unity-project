5bbe0401bf45d245294002c002ba0e66d7469987
jiangyukunok <yj297@cornell.edu>
2017-10-02 05:57:30 +0100

    use enum to generate more reasonal lanes

--- Assets/Scripts/LaneSpawner.cs
+++ Assets/Scripts/LaneSpawner.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LaneSpawner : MonoBehaviour {
-	public GameObject[] lanePrefabs;
+enum LaneType {Safe, Danger};
 
+public class LaneSpawner : MonoBehaviour {
+	public GameObject[] safeLanePrefabs;
+	public GameObject[] dangerousPrefabs;
+	LaneType lastLaneType = LaneType.Safe;
 	// Use this for initialization
 	void Start () {
 		int offset = 0;
@@ -12,13 +15,29 @@ public class LaneSpawner : MonoBehaviour {
 			offset += 1000;
 		}
 	}
-
+		
 	void CreateRandomLane(float offset){
-		int laneIndex = Random.Range (0, lanePrefabs.Length);
-		var lane = Instantiate (lanePrefabs [laneIndex]);
+		GameObject lane;
+		if (lastLaneType == LaneType.Safe) {
+			if (Random.value < 0.5) {
+				lane = InstantiateRandomLane (safeLanePrefabs);
+			} else {
+				lane = InstantiateRandomLane (dangerousPrefabs);
+				lastLaneType = LaneType.Danger;
+			}
+		} else {
+			lane = InstantiateRandomLane (safeLanePrefabs);
+			lastLaneType = LaneType.Safe;
+		}
 		lane.transform.SetParent (transform, false);//.parent = transform;
 		lane.transform.Translate (0, 0, offset);
 	}
+
+	GameObject InstantiateRandomLane(GameObject[] lanes){
+		int laneIndex = Random.Range (0, lanes.Length);
+		return Instantiate (lanes [laneIndex]);
+	}
+
 
 	// Update is called once per frame
 	void Update () {
